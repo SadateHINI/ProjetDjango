116fd30f685c2a9c6c062681fc7b4c18da2c1a70
Sadate HINI <mohamed.hini@ipnetinstitute.com>
2023-08-27 22:51:17 +0000

    Second

---
+++ Base_de_donner_django.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE projet_django3;
+use projet_django3;
+SELECT * FROM djangoapp_user;
+SELECT * FROM djangoApp_matiere;
+SELECT * FROM djangoApp_cour;
+SELECT * FROM djangoApp_filiere;
+SELECT * FROM djangoApp_etudiant;
+SELECT * FROM djangoapp_user_matiere_enseignant;
+SELECT * FROM djangoapp_user_matiere_enseignant;
+SELECT * FROM djangoApp_filiere_matieres;
+SELECT * FROM djangoApp_projet;
+SELECT * FROM djangoApp_devoir;
+SELECT * FROM djangoapp_projettest;
+SELECT * FROM djangoapp_filiere_matieres;
+SELECT * FROM djangoapp_mdjangoapp_projettest;
+SELECT * FROM djangoapp_matieretest;
+INSERT INTO  djangoapp_matieretest(nomMatiere) Values("DJANGO")
+
+
+
